981e01a9604192e4023cf609149db9cf914ab641
Indu Ilanchezian <induilanchezian@Indus-MacBook-Air.local>
2022-12-02 21:09:32 +0100

    Formatted and isorted py files

--- classifier_app/app.py
+++ classifier_app/app.py
@@ -1,15 +1,16 @@
 import streamlit as st
-from PIL import Image
-from clf import predict
-from munch import munchify
 import yaml
+from munch import munchify
+from PIL import Image
 
-conf = 'configs/default.yaml'
-with open(conf, 'r') as file:
+from clf import predict
+
+conf = "configs/default.yaml"
+with open(conf, "r") as file:
     cfg = yaml.load(file, Loader=yaml.FullLoader)
 cfg = munchify(cfg)
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option("deprecation.showfileUploaderEncoding", False)
 
 st.title("Diabetic Retinopathy Detection from retinal fundus")
 st.write("")
@@ -18,7 +19,7 @@ file_up = st.file_uploader("Upload an image", type="png")
 
 if file_up is not None:
     image = Image.open(file_up)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
     st.write("")
     st.write("Just a second...")
     labels = predict(file_up, cfg)
--- classifier_app/clf.py
+++ classifier_app/clf.py
@@ -1,26 +1,29 @@
-from torchvision import models, transforms
 import torch
+import torch.nn.functional as F
 from PIL import Image
 from torch import nn
-import torch.nn.functional as F
+from torchvision import models, transforms
+
 
 def predict(image_path, cfg):
-    checkpoint = ('resnet50_models/best_validation_weights_acc.pt')
+    checkpoint = "resnet50_models/best_validation_weights_acc.pt"
 
     num_class = cfg.data.num_classes
     resnet = models.resnet50(pretrained=True)
     resnet.fc = nn.Linear(resnet.fc.in_features, num_class)
 
-    weights = torch.load(checkpoint, map_location=torch.device('cpu'))
+    weights = torch.load(checkpoint, map_location=torch.device("cpu"))
     resnet.load_state_dict(weights, strict=True)
 
-    #https://pytorch.org/docs/stable/torchvision/models.html
-    transform = transforms.Compose([
-    transforms.Resize((cfg.data.input_size)),
-    transforms.CenterCrop(cfg.data.input_size),
-    transforms.ToTensor(),
-    transforms.Normalize(cfg.data.mean, cfg.data.std)
-    ])
+    # https://pytorch.org/docs/stable/torchvision/models.html
+    transform = transforms.Compose(
+        [
+            transforms.Resize((cfg.data.input_size)),
+            transforms.CenterCrop(cfg.data.input_size),
+            transforms.ToTensor(),
+            transforms.Normalize(cfg.data.mean, cfg.data.std),
+        ]
+    )
 
     img = Image.open(image_path)
     batch_t = torch.unsqueeze(transform(img), 0)
@@ -28,7 +31,7 @@ def predict(image_path, cfg):
     resnet.eval()
     out = resnet(batch_t)
 
-    with open('dr_classes.txt') as f:
+    with open("dr_classes.txt") as f:
         classes = [line.strip() for line in f.readlines()]
 
     prob = torch.nn.functional.softmax(out, dim=1)[0]
